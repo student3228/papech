a7891a8d6dbe7e3cedbfea9892e9b144caf39bdb
Egorka <student3228@gmail.com>
2017-11-30 14:04:10 +0300

    add function mypow()

--- funcs.cpp
+++ funcs.cpp
@@ -13,3 +13,12 @@ double MyMul(double a, double b) {
 double MyDiv(double a, double b) { 
 	return a / b;
 }
+int MyPow(int num, int n)
+{
+	int res = num;
+	for (int i = 1; i < n; i++)
+	{ 
+		res *= num; 
+	}
+	return res;
+}
