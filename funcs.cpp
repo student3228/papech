a2c58e0c8bbe1c2930e792561d242b7777901d1b
Egorka <student3228@gmail.com>
2017-11-30 14:09:25 +0300

    add finction mypow() v.2

--- funcs.cpp
+++ funcs.cpp
@@ -1,6 +1,7 @@
 //funcs.cpp
 #include "stdafx.h"
 #include "funcs.h"
+#include <cmath>
 double MySum(double a, double b) {
 	return a + b; 
 }
@@ -12,4 +13,8 @@ double MyMul(double a, double b) {
 }
 double MyDiv(double a, double b) { 
 	return a / b;
+}
+double MyPow(double a, double b) 
+{
+	return exp(b * log(a)); 
 }
